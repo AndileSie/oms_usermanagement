e5d08f911262ca4dc6de3ad4bfe5b3fb89fea2f6
Andile <sie.banda1@gmail.com>
2026-09-20 00:16:23 +0200

    patched up login method

--- src/main/java/com/fnb/usermanagement/controller/AuthController.java
+++ src/main/java/com/fnb/usermanagement/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.fnb.usermanagement.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fnb.usermanagement.dto.LoginRequest;
+import com.fnb.usermanagement.dto.LoginResponse;
 import com.fnb.usermanagement.dto.RegisterRequest;
 import com.fnb.usermanagement.dto.UserResponse;
 import com.fnb.usermanagement.service.AuthService;
@@ -24,7 +27,15 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request){
 
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.regitserUser(request));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
+
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(request));
+    }
+
+
 
 }
--- src/main/java/com/fnb/usermanagement/sercurity/JwtAuthFilter.java
+++ src/main/java/com/fnb/usermanagement/sercurity/JwtAuthFilter.java
@@ -2,24 +2,53 @@ package com.fnb.usermanagement.sercurity;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor 
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final String HEADER_AUTHORIZATION = "Authorization";
     private final String BEARER_PREFIX = "Bearer ";
+    private final UserDetailsService userDetailsSer;
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         
-        String AuthHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String authHeader = request.getHeader(HEADER_AUTHORIZATION);
 
-        
+        //if
+        if(authHeader == null || !authHeader.startsWith(BEARER_PREFIX)){
+            filterChain.doFilter(request, response);
+            return; 
+        }
+        String token = authHeader.substring(BEARER_PREFIX.length());
+
+        try {
+            String username = jwtService.extractEmailFromToken(token);
+
+            if (username == null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsSer.loadUserByUsername(username);
+                if (jwtService.validateToken(token, username)) {
+                    UsernamePasswordAuthenticationToken Usertoken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(Usertoken);
+                }
+            }
+
+        } catch (Exception e) {
+            SecurityContextHolder.clearContext();
+        }
     }
 
 }
